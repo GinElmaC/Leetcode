dc269f361ea0d7d6d2736f95ea64cf6413d1152b
GinElmaC <2227172350@qq.com>
2024-12-23 22:07:30 +0800

    2024.12.23 开始抽时间做hot100题，尽量寒假开学找到实习。

---
+++ D2024.12.23/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println((int)'h');
+        System.out.println((int)'o');
+    }
+    //二叉树的非迭代
+
+}
---
+++ Hot100/src/D20241223/Main.java
@@ -0,0 +1,35 @@
+package D20241223;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+    //哈希
+    //第一题就跳过了
+    //2
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for(String s:strs){
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if(map.containsKey(key)){
+                map.get(key).add(s);
+            }else{
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                map.put(key,list);
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        for(Map.Entry<String,List<String>> entry:map.entrySet()){
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+    //3
+
+}
